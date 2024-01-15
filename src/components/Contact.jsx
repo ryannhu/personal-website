3835ff4fd10910177a46c38a4c545a88d99c4412
Ryan <ryannhu3@gmail.com>
2024-01-15 17:21:13 -0500

    contact add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import '../styles/Contact.css'; // Assuming you have a CSS file for styles
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import "../styles/Contact.css"; // Assuming you have a CSS file for styles
 
 const Contact = () => {
   return (
@@ -9,14 +10,32 @@ const Contact = () => {
       <h1>4 - Contact Me</h1>
       <p>I would love to get in touch, feel free to reach out!</p>
       <div className="contact-links">
-        <a 
+        <a
           href="https://www.github.com/ryannhu"
           aria-label="Visit my GitHub profile"
           target="_blank"
           rel="noopener noreferrer"
-          className="github-link"
+          className="contact-link"
         >
-          <FontAwesomeIcon icon={faGithub} size="5x"/>
+          <FontAwesomeIcon icon={faGithub} size="5x" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ryannhu"
+          aria-label="Visit my LinkedIn profile"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="contact-link"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="5x" />
+        </a>
+        <a
+          href="mailto:ryannhu3@gmail.com"
+          aria-label="Email me"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="contact-link"
+        >
+          <FontAwesomeIcon icon={faEnvelope} size="5x" />
         </a>
       </div>
     </div>
